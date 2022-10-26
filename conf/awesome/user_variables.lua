6068880c38cdb169b225bfffcd28016defeb35b3
Diego Solis <Endgame189@outlook.com>
2022-10-26 18:11:57 -0500

    Add files via upload

---
+++ conf/awesome/user_variables.lua
@@ -0,0 +1,18 @@
+return {
+	widget = {
+		weather = {
+			--- API Key
+			key = "e49ae78c30d6e842a2841907db176af6",
+			--- Coordinates
+			coordinates = {
+				"38.5003", --- lat
+				"-98.5006", --- lon
+			},
+		},
+
+		--- Github activity
+		github = {
+			username = "d-solis",
+		},
+	},
+}
