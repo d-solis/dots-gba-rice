2e53c323b337fcb12f16d8a34de07995581b25c0
Diego Solis <Endgame189@outlook.com>
2022-10-26 18:03:36 -0500

    Add files via upload

---
+++ conf/zsh/theme.zsh
@@ -0,0 +1,41 @@
+##
+## Aesthetic Night Colorscheme
+##
+
+# special
+foreground='#e3e6eb'
+background='#101419'
+
+# black
+color0='#101419'
+color8='#101419'
+
+# red
+color1='#df5b61'
+color9='#e8646a'
+
+# green
+color2='#78b892'
+color10='#81c19b'
+
+# yellow
+color3='#de8f78'
+color11='#e79881'
+
+# blue
+color4='#6791c9'
+color12='#709ad2'
+
+# magenta
+color5='#bc83e3'
+color13='#c58cec'
+
+# cyan
+color6='#67afc1'
+color14='#70b8ca'
+
+# white
+color7='#e4e6e7'
+color15='#f2f4f5'
+
+# vim:filetype=zsh
